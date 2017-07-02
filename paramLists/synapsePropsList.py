aacc09893cfa45044f49bba5a6139315800c07bf
Ajayrama Kumaraswamy <ajkumaraswamy@tutamail.com>
2017-07-02 21:15:58 +0900

    DL-Int-2 synapse params changed to work with 100ms IPI

--- paramLists/synapsePropsList.py
+++ paramLists/synapsePropsList.py
@@ -4,6 +4,7 @@ DLInt1_syn_try1 = dict(wSyn=[5000, 400] * units.usiemens,
                     Esyn=[0, -80] * units.mvolt,
                     tau1=[4, 4] * units.ms, tau2=[4.5, 8] * units.ms,
                     delay=[6, 10] * units.ms)
+
 DLInt1_syn_try2 = dict(wSyn=[15000, 1500] * units.nsiemens,
                     Esyn=[0, -80] * units.mvolt,
                     tau1=[4, 4] * units.ms, tau2=[4.5, 8] * units.ms,
@@ -19,12 +20,12 @@ DLInt1_syn_try2_i = dict(wSyn=160 * units.nsiemens,
                     tau1=12 * units.ms, tau2=15 * units.ms,
                     delay=10 * units.ms)
 
-DLInt2_syn_try2 = dict(wSyn=80 * units.nsiemens,
+DLInt2_syn_try2 = dict(wSyn=75 * units.nsiemens,
                     Esyn=0 * units.mvolt,
                     tau1=0.5 * units.ms, tau2=2 * units.ms,
                     delay=5 * units.ms)
 
-DLInt1_DLInt2_try1 = dict(wSyn=180 * units.nsiemens,
+DLInt1_DLInt2_try1 = dict(wSyn=500 * units.nsiemens,
                     Esyn=-80 * units.mvolt,
-                    tau1=12 * units.ms, tau2=15 * units.ms,
+                    tau1=7 * units.ms, tau2=8 * units.ms,
                     delay=3 * units.ms)
